eacc890210f6ca977c1d17479a4d5d2049ecacf7
Muhammad Ali khan <alykhan2212@gmail.com>
2020-06-16 03:35:00 +0500

    new component students added

---
+++ src/students.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Students(props){
+
+    return(
+        <div>
+            <h2>Student Name {props.studentName}</h2>
+        </div>
+    
+    
+    );
+}
+
+export default Students;
